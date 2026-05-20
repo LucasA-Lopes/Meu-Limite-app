49123e6689a4f81e679e8115d7069f4ea66dd8da
lucas viana <luccasviana21@gmail.com]>
2026-05-20 20:32:30 -0300

    Adicionado biblioteca gifted charts para o gráfico 'Total Gasto', Adição de legendase formatação do gráfico usando funções UseEffected para carregar os dados gastos

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button, TextInput} from 'react-native'; /*Importação dos componentes necessários*/
+/*import React, {useEffect, useSate} from 'react';
+import { View, Text, StyleSheet, Button, TextInput} from 'react-native'; 
 
 /*Função Topo Dashboard*/
-export function DashboardTxt() {   
+/*export function DashboardTxt() {   
   return (
     <View>
       <Text>Meu Limite App</Text>
@@ -13,7 +13,7 @@ export function DashboardTxt() {
 }
 
 export default function Dashboard({navigation}) {                           /*Função padrão para conteudo da página, navegação e Botão*/                        
-  return (
+/*  return (
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
       <DashboardTxt/>
 
@@ -34,5 +34,221 @@ export default function Dashboard({navigation}) {                           /*Fu
 
     </View>
   );
+}*/
+
+import React, {
+  useEffect,
+  useState
+} from 'react';
+
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Dimensions
+} from 'react-native';
+
+import {
+  collection,
+  getDocs
+} from 'firebase/firestore';
+
+import { db } from '../firebase/firebaseConfig';
+
+import { PieChart }
+from 'react-native-chart-kit';
+
+export default function Dashboard({ navigation }) {
+
+
+  const [gastos, setGastos] = useState([]);     /*Guarda dos Dados*/
+  const screenWidth =
+  Dimensions.get('window').width;
+
+  async function carregarGastos() {
+
+    const querySnapshot =
+      await getDocs(                                            /*Busca dos Dados*/
+        collection(db, 'expenses')
+      );
+
+    const lista = [];
+
+    querySnapshot.forEach((doc) => {
+
+      lista.push({
+        id: doc.id,
+        ...doc.data()
+      });
+
+    });
+
+    setGastos(lista);
+
+  }
+
+  useEffect(() => {
+
+    const unsubscribe =
+      navigation.addListener('focus', () => {
+
+        carregarGastos();
+
+      });
+
+    return unsubscribe;
+
+  }, [navigation]);
+
+  let credito = 0;
+  let debito = 0;
+  let pix = 0;
+
+  gastos.forEach((item) => {
+
+    const valor =
+      parseFloat(item.valor) || 0;
+
+    if(item.pagamento.toLowerCase() === 'crédito' ) {
+      credito += valor;
+    }
+
+    else if(item.pagamento.toLowerCase() === 'débito') {
+      debito += valor;
+    }
+
+    else if(item.pagamento.toLowerCase() === 'pix') {
+      pix += valor;
+    }
+
+  });
+
+  return (
+
+    <View style={{
+      flex:1,
+      padding:20
+    }}>
+
+      <Text style={{
+        fontSize:24,
+        marginBottom:20
+      }}>
+        Meu Limite
+      </Text>
+
+
+  <PieChart
+    data={[
+      {
+        name: 'Crédito',
+        population: credito,
+        color: 'black',
+        legendFontColor: '#000',
+        legendFontSize: 14
+      },
+
+      {
+        name: 'Débito',
+        population: debito,
+        color: 'blue',
+        legendFontColor: '#000',
+        legendFontSize: 14
+      },
+
+      {
+        name: 'Pix',
+        population: pix,
+        color: 'purple',
+        legendFontColor: '#000',
+        legendFontSize: 14
+      }
+    ]}
+
+  width={screenWidth - 40}
+
+  height={220}
+
+  chartConfig={{
+    color: () => '#000'
+  }}
+
+  accessor={'population'}
+
+  backgroundColor={'transparent'}
+
+  paddingLeft={'15'}
+
+  absolute
+/>
+
+      <View style={{
+      alignItems:'center',
+      marginBottom:20
+    }}>
+
+</View>
+
+      <Button
+        title="Adicionar Gasto"
+        onPress={() =>
+          navigation.navigate('AddExpense')
+        }
+      />
+
+      <Text style={{
+        marginTop:20,
+        marginBottom:10,
+        fontSize:18
+      }}>
+        Lista de Gastos
+      </Text>
+
+      <FlatList                                                           /*Mostrar os dados em forma de Lista*/
+        data={gastos}
+
+        keyExtractor={(item) => item.id}
+
+        renderItem={({ item }) => (
+
+          <View style={{
+            borderWidth:1,
+            padding:15,
+            marginBottom:10,
+            borderRadius:10
+          }}>
+
+            <Text>
+              Valor: {item.valor}
+            </Text>
+
+            <Text>
+              Pagamento: {item.pagamento}
+            </Text>
+
+            <Text>
+              Data: {item.data}
+            </Text>
+            <Button
+              title="Editar"
+              onPress={() =>
+                navigation.navigate(
+                  'EditExpense',
+                  { gasto: item }
+                )
+              }
+            />
+            
+          </View>
+          
+
+        )}
+      />
+
+    </View>
+  );
 }
+
+
 
--- src/screens/EditExpense.js
+++ src/screens/EditExpense.js
@@ -1,15 +1,149 @@
-import React from 'react';
+/*import React from 'react';
 import { View, Text, Button} from 'react-native';
 
 export default function AddExpense({navigation}) {
   return (
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
       <Text>Tela Adicionar Gasto</Text>
-      <br></br>
       <Button
         title="Voltar"
         onPress={() => navigation.goBack()}
       />
     </View>
   );
+}*/
+
+import React, {
+  useState
+} from 'react';
+
+import {
+  View,
+  Text,
+  Button,
+  TextInput
+} from 'react-native';
+
+import {
+  doc,
+  updateDoc,
+  deleteDoc
+} from 'firebase/firestore';
+
+import { db } from '../firebase/firebaseConfig';
+
+export default function EditExpense({
+  navigation,
+  route
+}) {
+
+  const { gasto } = route.params;
+
+  const [valor, setValor] =
+    useState(gasto.valor);
+
+  const [pagamento, setPagamento] =
+    useState(gasto.pagamento);
+
+  const [data, setData] =
+    useState(gasto.data);
+
+  async function atualizarGasto() {
+
+    const gastoRef =
+      doc(db, 'expenses', gasto.id);
+
+    await updateDoc(gastoRef, {
+      valor,
+      pagamento,
+      data
+    });
+
+    navigation.goBack();
+
+  }
+
+  async function deletarGasto() {
+
+    const gastoRef =
+      doc(db, 'expenses', gasto.id);
+
+    await deleteDoc(gastoRef);
+
+    navigation.goBack();
+
+  }
+
+  return (
+
+    <View style={{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center"
+    }}>
+
+      <Text style={{
+        fontSize:20,
+        marginBottom:20
+      }}>
+        Editar Gasto
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        value={valor}
+        onChangeText={setValor}
+      />
+
+      <TextInput
+        style={styles.input}
+        value={pagamento}
+        onChangeText={setPagamento}
+      />
+
+      <TextInput
+        style={styles.input}
+        value={data}
+        onChangeText={setData}
+      />
+
+      <Button
+        title="Atualizar"
+        onPress={atualizarGasto}
+      />
+
+      <View style={{marginTop:20}}>
+
+        <Button
+          title="Deletar"
+          onPress={deletarGasto}
+        />
+
+      </View>
+
+      <View style={{marginTop:20}}>
+
+        <Button
+          title="Voltar"
+          onPress={() =>
+            navigation.goBack()
+          }
+        />
+
+      </View>
+
+    </View>
+  );
 }
+
+const styles = {
+
+  input: {
+    borderWidth:1,
+    width:250,
+    padding:10,
+    marginBottom:15,
+    borderRadius:8
+  }
+
+};
